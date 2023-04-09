63822a2e491d05ef7d3cfdf2201253dca2c4f837
soshanHANUMANTHU <soshandora0729@gmail.com>
2023-04-09 19:27:10 +0530

    First Commit

---
+++ demo.java
@@ -0,0 +1,20 @@
+package java_baskics;
+
+public class MyUtils {
+	public static void JustSampleMethod(String args) {
+		System.out.println("hello soshan concentrate more on methods"+args);
+	}
+	public static void JustSampleMethod(int args) {
+		System.out.println("integer value passed");
+	}
+	public static void SumTwoNumbers(int firstargs,int seccondargs) {
+		System.out.println(firstargs+seccondargs);
+	}
+	public static int  substractTen(int arg) {
+		int value=(arg-10);
+		return value;
+	}
+	public void  printName() {
+		System.out.println("hello sohan dora");
+	}
+}
